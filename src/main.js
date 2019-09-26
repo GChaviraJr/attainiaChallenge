d491f8b076492cce790322191f8e37f25d5231d2
Lupe <lupec_se@yahoo.com>
2019-09-26 09:41:39 -0700

    added node http

--- src/main.js
+++ src/main.js
@@ -6,6 +6,26 @@ import { routes } from './routes'
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 
+const http = require('http')
+const fs = require('fs')
+const httpPort = 80
+
+http.createServer((req, res) => {
+  fs.readFile('index.htm', 'utf-8', (err, content) => {
+    if (err) {
+      console.log('We cannot open "index.htm" file.')
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
+
+    res.end(content)
+  })
+}).listen(httpPort, () => {
+  console.log('Server listening on: http://localhost:%s', httpPort)
+})
+
 const router = new VueRouter({
   routes
 })
