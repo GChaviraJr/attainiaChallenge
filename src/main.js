05cb154af80f0b759b34f8be8aee9b998a6887a1
Lupe <lupec_se@yahoo.com>
2019-09-26 09:27:44 -0700

    added create server for node

--- src/main.js
+++ src/main.js
@@ -3,10 +3,31 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import { routes } from './routes'
 
+const http = require('http')
+const fs = require('fs')
+const httpPort = 80
+
+http.createServer((req, res) => {
+  fs.readFile('index.htm', 'utf-8', (err, content) => {
+    if (err) {
+      console.log('We cannot open "index.htm" file.')
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
+
+    res.end(content)
+  })
+}).listen(httpPort, () => {
+  console.log('Server listening on: http://localhost:%s', httpPort)
+})
+
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
